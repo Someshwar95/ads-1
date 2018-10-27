bfa61f9766345ee86604b9a9acadd5a58e563d93
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-10-27 12:52:47 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads-1 assignment/m24/1/Solution.java
+++ ads-1 assignment/m24/1/Solution.java
@@ -1,25 +1,70 @@
-import java.util.Scanner;
+/**.
+ * Class for student data.
+ */
 class StudentData {
-	private int roll_number;
-	private String student_name;
-	private double total_marks;
-	StudentData(int rollnumber, String studentname, double totalmarks) {
-		roll_number = rollnumber;
-		student_name = studentname;
-		total_marks = totalmarks;
-	}
-	public int getRollnumber() {
-		return roll_number;
-	}
-	public String getStudentname() {
-		return student_name;
-	}
-	public double getTotalmarks() {
-		return total_marks;
-	}
+    /**.
+     * { var_description }
+     */
+    private int n;
+    /**.
+     * { var_description }
+     */
+    private int roll_number;
+    /**.
+     * { var_description }
+     */
+    private String student_name;
+    /**.
+     * { var_description }
+     */
+    private double total_marks;
+    /**.
+     * Constructs the object.
+     *
+     * @param      rollnumber   The rollnumber
+     * @param      studentname  The studentname
+     * @param      totalmarks   The totalmarks
+     */
+    StudentData(int rollnumber, String studentname, double totalmarks) {
+        roll_number = rollnumber;
+        student_name = studentname;
+        total_marks = totalmarks;
+    }
+    /**.
+     * Gets the rollnumber.
+     *
+     * @return     The rollnumber.
+     */
+    public int getRollnumber() {
+        return roll_number;
+    }
+    /**.
+     * Gets the studentname.
+     *
+     * @return     The studentname.
+     */
+    public String getStudentname() {
+        return student_name;
+    }
+    /**.
+     * Gets the totalmarks.
+     *
+     * @return     The totalmarks.
+     */
+    public double getTotalmarks() {
+        return total_marks;
+    }
 }
+/**.
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
-		
-	}
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        
+    }
 }
