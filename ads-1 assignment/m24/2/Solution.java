6ee0c5511676c1aac5a1a37d4afd9d208c75ec2f
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-10-27 12:53:52 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads-1 assignment/m24/2/Solution.java
+++ ads-1 assignment/m24/2/Solution.java
@@ -1,39 +1,83 @@
-import java.util.*;
+import java.util.Scanner;
+/**.
+ * Class for student data.
+ */
 class StudentData {
-	private int n;
-	private int roll_number;
-	private String student_name;
-	private double total_marks;
-	StudentData(int rollnumber, String studentname, double totalmarks) {
-		roll_number = rollnumber;
-		student_name = studentname;
-		total_marks = totalmarks;
-	}
-	public int getRollnumber() {
-		return roll_number;
-	}
-	public String getStudentname() {
-		return student_name;
-	}
-	public double getTotalmarks() {
-		return total_marks;
-	}
+    /**.
+     * { var_description }
+     */
+    private int n;
+    /**.
+     * { var_description }
+     */
+    private int roll_number;
+    /**.
+     * { var_description }
+     */
+    private String student_name;
+    /**.
+     * { var_description }
+     */
+    private double total_marks;
+    /**.
+     * Constructs the object.
+     *
+     * @param      rollnumber   The rollnumber
+     * @param      studentname  The studentname
+     * @param      totalmarks   The totalmarks
+     */
+    StudentData(int rollnumber, String studentname, double totalmarks) {
+        roll_number = rollnumber;
+        student_name = studentname;
+        total_marks = totalmarks;
+    }
+    /**.
+     * Gets the rollnumber.
+     *
+     * @return     The rollnumber.
+     */
+    public int getRollnumber() {
+        return roll_number;
+    }
+    /**.
+     * Gets the studentname.
+     *
+     * @return     The studentname.
+     */
+    public String getStudentname() {
+        return student_name;
+    }
+    /**.
+     * Gets the totalmarks.
+     *
+     * @return     The totalmarks.
+     */
+    public double getTotalmarks() {
+        return total_marks;
+    }
 }
-
+/**.
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int roll_number = sc.nextInt();
-		String student_name = sc.nextLine();
-		double total_marks = sc.nextDouble();
-		if (total_marks >= 70.32 || total_marks <= 60.0) {
-			System.out.println(student_name);
-		}
-		if (total_marks <= 32.3) {
-			System.out.println(student_name);
-		}
-		if (total_marks >= 85.5) {
-			System.out.println(student_name);
-		}
-	}
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int roll_number = sc.nextInt();
+        String student_name = sc.nextLine();
+        double total_marks = sc.nextDouble();
+        if (total_marks >= 70.32 || total_marks <= 60.0) {
+            System.out.println(student_name);
+        }
+        if (total_marks <= 32.3) {
+            System.out.println(student_name);
+        }
+        if (total_marks >= 85.5) {
+            System.out.println(student_name);
+        }
+    }
 }
